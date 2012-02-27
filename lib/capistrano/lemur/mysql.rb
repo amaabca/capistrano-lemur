4000e3b3929ec18a8f8a230369025671b701f51f
Jordan Babe <jordan.babe@ama.ab.ca>
2012-02-27 11:00:03 -0700

    Added recipe to create mysql users

---
+++ lib/capistrano/lemur/mysql.rb
@@ -0,0 +1,52 @@
+Capistrano::Configuration.instance.load do
+
+  namespace :mysql do
+  
+    desc "Create default db and user. TODO: Make this idempotent"
+    task :create_users, :roles => :db, :except => { :no_release => true } do
+  
+     db_config = YAML::load_file("config/database.yml")
+     db_user = db_config[rails_env.to_s]["username"]
+     db_password = db_config[rails_env.to_s]["password"]
+     db_name = db_config[rails_env.to_s]["database"]
+   
+     sudo "ls -al" #So we don't get prompted on the line below.....  
+     db_root_password = capture("sudo cat /var/cache/local/preseeding/mysql-server.seed").split(" ")[3]
+   
+   
+     servers = roles[:app].collect {|r| r.host}
+     servers << "localhost"
+
+
+     servers.each do |server|
+       
+       check_user  = capture("mysql --user=root -p#{db_root_password} -B -N -e \"select count(*) from mysql.user where host = '#{server}' and user = '#{db_user}';\"").chomp.to_i
+       run "mysql --user=root -p#{db_root_password} -e \"CREATE USER '#{db_user}'@'#{server}' IDENTIFIED BY '#{db_password}'\"" unless check_user == 1
+       run "mysql --user=root -p#{db_root_password} -e \"GRANT CREATE ON *.* TO '#{db_user}'@'#{server}'\""
+       run "mysql --user=root -p#{db_root_password} -e \"GRANT ALL PRIVILEGES ON #{db_name}.* TO '#{db_user}'@'#{server}' IDENTIFIED BY '#{db_password}' WITH GRANT OPTION\""
+       run "mysql --user=root -p#{db_root_password} -e \"FLUSH PRIVILEGES;\""
+     end
+   
+    end
+
+    desc "Capistrano override: Setup application schema"
+    task :setup, :only => {:primary => true}, :except => { :no_release => true } do  
+      run "cd #{current_path}; bundle exec rake RAILS_ENV=#{rails_env} db:create"
+    end
+  
+    desc "Capistrano override: db:migration"
+    task :migrate, :only => {:primary => true}, :except => { :no_release => true } do
+      run "cd #{current_path}; bundle exec rake RAILS_ENV=#{rails_env} db:migrate"
+    end
+  
+    desc "Capistrano override: seed the database on already deployed code"
+    task :seed, :only => {:primary => true}, :except => { :no_release => true } do
+      confirm = Capistrano::CLI.ui.ask "This is a dangerous task. Type Y to continue."
+      exit unless confirm.downcase == 'y'
+      run "cd #{current_path}; bundle exec rake RAILS_ENV=#{rails_env} db:seed"
+    end
+  
+  
+  end
+
+end
