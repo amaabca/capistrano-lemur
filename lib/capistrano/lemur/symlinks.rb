af3a70b7405d9f4eff6ac86d7ebd6f367d1cd03e
Jordan Babe <jordan.babe@ama.ab.ca>
2012-02-28 16:36:22 -0700

    create symlinks

---
+++ lib/capistrano/lemur/symlinks.rb
@@ -0,0 +1,12 @@
+Capistrano::Configuration.instance.load do
+  
+  namespace :symlinks do
+    desc "Iterate over 'symlinks' hash: { '/path/to/src1' => '/path/to/dest2', '/path/to/src2' => '/path/to/dest2'}"
+    task :default do      
+      all_symlinks.each_pair do |src, dest|
+        puts "if [! -L '#{dest}']; then ln -s #{src} #{dest}; fi"
+      end
+    end
+  end
+
+end
