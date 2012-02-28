e394079eaca9e311458af479ced518a808dd2501
Jordan Babe <jordan.babe@ama.ab.ca>
2012-02-28 09:11:30 -0700

    moved amaabca project unicorn tasks into gem
    * Moving our unicorn code into a gem

---
+++ lib/capistrano/lemur/unicorn.rb
@@ -0,0 +1,49 @@
+Capistrano::Configuration.instance.load do
+
+      # TODO: make this more generic once we start using in multiple apps
+      namespace :deploy do
+
+        # override default tasks to make capistrano happy
+        desc "Start unicorn"
+        task :start, :roles => :app do
+        #  start_primary
+          start_secondary unless (find_servers :roles => :app, :except => {:primary => true}).empty?
+        end
+
+        desc "Start unicorn on primary server"
+        task :start_primary, :roles => :app, :only => { :primary => true } do
+          run "cd #{current_path} && server=primary bundle exec unicorn -c #{current_path}/config/unicorn.rb -E #{rails_env} -D"
+        end
+  
+        desc "Start unicorn on secondary server(s)"
+        task :start_secondary, :roles => :app, :except => { :primary => true } do
+          run "cd #{current_path} && server=secondary bundle exec unicorn -c #{current_path}/config/unicorn.rb -E #{rails_env} -D"
+        end
+  
+        desc "Start unicorn on primary server"
+        task :stop_primary, :roles => :app, :only => { :primary => true } do
+          run "kill -QUIT `cat #{current_path}/tmp/pids/unicorn.pid`"
+          sleep 2
+          start_primary
+        end
+  
+        desc "Start unicorn on secondary server(s)"
+        task :stop_secondary, :roles => :app, :except => { :primary => true } do
+          run "kill -QUIT `cat #{current_path}/tmp/pids/unicorn.pid`"
+          sleep 2
+          start_secondary
+        end
+
+        desc "Kick unicorn"
+        task :restart, :roles => :app do
+          run "kill -USR2 `cat #{current_path}/tmp/pids/unicorn.pid`"
+        end
+
+        desc "Kill a unicorn"
+        task :stop, :roles => :app do
+          run "kill -QUIT `cat #{current_path}/tmp/pids/unicorn.pid`"
+        end
+   end
+  
+
+end
