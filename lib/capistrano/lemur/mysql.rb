cba447154f1c27653e99e44261bef0d703a4b7cc
Jordan Babe <jordan.babe@ama.ab.ca>
2012-03-02 09:24:03 -0700

    This should have been 2 commits. Whatever:
    so that I can specify the host for mysql users
    
    so that there is a default Nginx virtual host file for the app

--- lib/capistrano/lemur/mysql.rb
+++ lib/capistrano/lemur/mysql.rb
@@ -3,23 +3,21 @@ Capistrano::Configuration.instance.load do
   namespace :mysql do
   
     desc "LMR Create default db and user. TODO: Make this idempotent"
-    task :create_users, :roles => :db, :except => { :no_release => true } do
+    task :create_users, :roles => :db, :only => {:primary => true}, :except => { :no_release => true } do
   
      db_config = YAML::load_file("config/database.yml")
      db_user = db_config[rails_env.to_s]["username"]
      db_password = db_config[rails_env.to_s]["password"]
      db_name = db_config[rails_env.to_s]["database"]
-   
-     sudo "ls -al" #So we don't get prompted on the line below.....  
-     db_root_password = capture("sudo cat /var/cache/local/preseeding/mysql-server.seed").split(" ")[3]
-   
-   
-     servers = roles[:app].collect {|r| r.host}
-     servers << "localhost"
+    
+     db_root_password = Capistrano::CLI.password_prompt("Mysql root user password (blank to use value created by Chef run)?") 
+     if db_root_password.to_s.length == 0
+       sudo "ls -al" #So we don't get prompted on the line below.....  
+       db_root_password = capture("sudo cat /var/cache/local/preseeding/mysql-server.seed").split(" ")[3]
+       puts "Use this password #{db_root_password} "
+     end
 
-
-     servers.each do |server|
-       
+     fetch(:mysql_server, roles[:app].collect {|r| r.host} + ["localhost"]).each do |server|       
        check_user  = capture("mysql --user=root -p#{db_root_password} -B -N -e \"select count(*) from mysql.user where host = '#{server}' and user = '#{db_user}';\"").chomp.to_i
        run "mysql --user=root -p#{db_root_password} -e \"CREATE USER '#{db_user}'@'#{server}' IDENTIFIED BY '#{db_password}'\"" unless check_user == 1
        run "mysql --user=root -p#{db_root_password} -e \"GRANT CREATE ON *.* TO '#{db_user}'@'#{server}'\""
@@ -46,7 +44,7 @@ Capistrano::Configuration.instance.load do
       run "cd #{current_path}; bundle exec rake RAILS_ENV=#{rails_env} db:seed"
     end
   
-  
+    before "deploy:cold", "mysql:create_users"
   end
 
 end
--- lib/capistrano/lemur/nginx.rb
+++ lib/capistrano/lemur/nginx.rb
@@ -1,8 +1,13 @@
 Capistrano::Configuration.instance.load do
   namespace :nginx do
     
-    set(:nginx_path) { "/etc/nginx" }
-    set(:nginx_cmd) {"/etc/init.d/nginx"}
+    set(:nginx_path)          { "/etc/nginx" }
+    set(:nginx_cmd)           { "/etc/init.d/nginx" }
+    set(:nginx_ports)         { Array("80") }
+    
+    set(:nginx_server_names) {
+      find_servers(:roles => :web).collect {|s| s.host}
+    }
 
     desc "LMR Copy application nginx config into sites-available and symlink into sites-enabled"
     task :setup, :roles => :web do
@@ -13,6 +18,19 @@ Capistrano::Configuration.instance.load do
         # Backup the old config and copy app config
         run "cp #{nginx_path}/sites-available/#{application} #{nginx_path}/sites-available/#{application}.#{Time.now.strftime("%Y%m%dT%H%M%S")} && cp #{current_path}/config/#{location} #{nginx_path}/sites-available/#{application}"
         run "if [ ! -L #{nginx_path}/sites-enabled/#{application} ]; then ln -s #{nginx_path}/sites-available/#{application} #{nginx_path}/sites-enabled/#{application}; fi"
+      else
+        puts "Nginx configuration change aborted"
+        exit
+      end
+    end
+    
+    desc "Generate Nginx virtual host file"
+    task :generate_config, :roles => :web do
+      confirm = Capistrano::CLI.ui.ask "This is a dangerous task. Type 'yes sir' to continue."
+      if confirm.downcase == 'yes sir'
+        config_file = ERB.new(fetch(:nginx_erb_template)).result(binding)
+        put config_file, "#{nginx_path}/sites-available/#{application}"
+        run "if [ ! -L /etc/nginx/sites-enabled/#{application} ]; then ln -s /etc/nginx/sites-available/#{application} /etc/nginx/sites-enabled/#{application}; fi"
       else
         puts "Nginx configuration change aborted"
         exit
@@ -50,4 +68,57 @@ Capistrano::Configuration.instance.load do
     end
 
   end  
+  
+  set(:nginx_erb_template) do
+    <<-NGINX  
+  upstream <%= application.to_s %> {
+    server unix:<%= shared_path.to_s %>/tmp/sockets/unicorn.sock fail_timeout=0;
+  } 
+
+  server {
+    listen <%= nginx_ports.join(" ") %>;
+    server_name <%= nginx_server_names.join(" ") %>;
+
+    root <%= current_path.to_s %>/public;
+
+    rewrite_log on;
+
+    access_log  /var/log/nginx/<%= application.to_s %>-access.log combined;
+    error_log   /var/log/nginx/<%= application.to_s %>-error.log;
+
+    location / {
+      #all requests are sent to the UNIX socket
+      proxy_pass  http://<%= application.to_s %>;
+      proxy_redirect     off;
+
+      proxy_set_header   Host             $host;
+      proxy_set_header   X-Real-IP        $remote_addr;
+      proxy_set_header   X-Forwarded-For  $proxy_add_x_forwarded_for;
+
+      client_max_body_size       10m;
+      client_body_buffer_size    128k;
+
+      proxy_connect_timeout      90;
+      proxy_send_timeout         90;
+      proxy_read_timeout         90;
+
+      proxy_buffer_size          4k;
+      proxy_buffers              4 32k;
+      proxy_busy_buffers_size    64k;
+      proxy_temp_file_write_size 64k;
+    }
+
+    # if the request is for a static resource, nginx should serve it directly
+    # and add a far future expires header to it, making the browser
+    # cache the resource and navigate faster over the website
+    # this probably needs some work with Rails 3.1's asset pipe_line
+    location ~ ^/(images|javascripts|stylesheets|system)/  {
+      root <%= current_path.to_s %>/public;
+      expires max;
+      break;
+    }
+  }
+  NGINX
+  end
+
 end
