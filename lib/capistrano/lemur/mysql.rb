6173d63a01eadf6621043aba8f98eb4eb5eda720
Jordan Babe <jordan.babe@ama.ab.ca>
2012-08-22 10:19:55 -0600

    Add some dumb cap tasks to create a mysql user and create a database

--- lib/capistrano/lemur/mysql.rb
+++ lib/capistrano/lemur/mysql.rb
@@ -35,6 +35,42 @@ Capistrano::Configuration.instance.load do
 
     end
 
+
+    desc "Just creates a mysql user"
+    task :create_user, :roles => :db_server, :except => { :no_release => true } do
+
+      db_server = Capistrano::CLI.ui.ask("Mysql server: ")
+      db_user  = Capistrano::CLI.ui.ask("Database user: ")
+      db_host = Capistrano::CLI.ui.ask("Database user: ") || "%"
+      db_password = Capistrano::CLI.password_prompt("Database user password: ")
+      db_name = Capistrano::CLI.ui.ask("Database name: ")
+
+     db_root_password = Capistrano::CLI.password_prompt("Mysql root user password (blank to use value created by Chef run)?")
+     if db_root_password.to_s.length == 0
+       usr = user
+       set :user, "root"
+       db_root_password = capture("cat /var/cache/local/preseeding/mysql-server.seed").split(" ")[3]
+       set :user, usr
+     end
+
+     check_user  = capture("mysql --user=root -p#{db_root_password} -B -N -e \"select count(*) from mysql.user where host = '#{db_host}' and user = '#{db_user}';\"").chomp.to_i
+     run "mysql --user=root -p#{db_root_password} -e \"CREATE USER '#{db_user}'@'%' IDENTIFIED BY '#{db_password}'\"" unless check_user == 1
+     run "mysql --user=root -p#{db_root_password} -e \"GRANT CREATE ON *.* TO '#{db_user}'@'#{db_host}'\""
+     run "mysql --user=root -p#{db_root_password} -e \"GRANT ALL PRIVILEGES ON #{db_name}.* TO '#{db_user}'@'#{db_host}' IDENTIFIED BY '#{db_password}' WITH GRANT OPTION\""
+     run "mysql --user=root -p#{db_root_password} -e \"FLUSH PRIVILEGES;\""
+    end
+
+    desc "Runs rake db:create"
+    task :create_db, :roles => :db_server, :except => { :no_release => true } do
+      foreman = Capistrano::CLI.ui.ask("Run task using foreman (Yn)?: ")
+      if !foreman.nil? && response.downcase == 'y'
+        run "cd #{current_path} && bin/foreman run bin/rake db:create"
+      else
+        run "cd #{current_path} && bin/rake db:create"
+      end
+    end
+
+
     before "deploy:cold", "mysql:create_users"
 
   end
